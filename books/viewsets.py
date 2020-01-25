eac1f4a0c59bc8175c9f1f09909d70bdc4ccfab2
otulakdominik <otulakdominik@gmail.com>
2020-01-25 14:15:22 +0100

    delete unused imports

--- books/viewsets.py
+++ books/viewsets.py
@@ -1,9 +1,7 @@
 import django_filters.rest_framework
-from django_filters import rest_framework as filters
 from rest_framework import viewsets
 from .serializers import BookSerializer
 from .models import Book
-from rest_framework import filters
 from .filters import BookFilter
 
 
